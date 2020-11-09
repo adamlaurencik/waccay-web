784c6645f682d7e24cda0285556d3c57634f8812
Adam Laurencik <laurencik@inqool.cz>
2020-11-09 01:19:55 +0100

    register schema validation bugfix

--- src/pages/Register/Register.schema.ts
+++ src/pages/Register/Register.schema.ts
@@ -8,10 +8,8 @@ const RegisterFormSchema = Yup.object<RegisterFormValues>({
   email: Yup.string().email().required(),
   password: Yup.string()
     .required()
-    .test(
-      'strength',
-      'Password is not strong enough',
-      (val) => zxcvbn(val as string).score >= MIN_PASSWORD_STRENGTH
+    .test('strength', 'Password is not strong enough', (val) =>
+      val ? zxcvbn(val).score > MIN_PASSWORD_STRENGTH : false
     ),
   username: Yup.string().required(),
 });
